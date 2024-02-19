bcb782a1934a369d81803eb8bd43a35b5f79a089
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2024-02-19 15:42:01 +0200

    Understanding Boolean operators

--- _100_daysJavaChallenge/src/Day1_To_10/Day2_ControlFlow.java
+++ _100_daysJavaChallenge/src/Day1_To_10/Day2_ControlFlow.java
@@ -1,4 +1,20 @@
 package Day1_To_10;
 
+import java.util.Scanner;
+
 public class Day2_ControlFlow {
+    public static void main(String[] args) {
+        int number1 = (int)(System.currentTimeMillis() % 10);
+        int number2 = (int)(System.currentTimeMillis() / 7 % 10);
+
+        //create a Scanner
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is " + number1 + " + " + number2 + "? ");
+        int number = input.nextInt();
+
+        int answer = number1 + number2;
+
+        System.out.println(number1 + " + " + number2 + " = " + answer + " is " + (number1 + number2 == answer));
+    }
 }
